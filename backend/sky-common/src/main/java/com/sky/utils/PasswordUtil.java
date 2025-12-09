39ca8343502964126f7ff3ec2dcc1038e62bd5a5
gengetsu <x1156798139@gmail.com>
2025-12-09 11:51:02 +0800

    refactor: Rename Setmeal to SetMeal for consistency

---
+++ backend/sky-common/src/main/java/com/sky/utils/PasswordUtil.java
@@ -0,0 +1,10 @@
+package com.sky.utils;
+
+/**
+ * @ClassName PasswordUtil
+ * @Description 密码工具类
+ * @author Gengetsu
+ * @dateTime 9/12/2025 上午11:15
+ * @version v1.0
+ */public class PasswordUtil {
+}
