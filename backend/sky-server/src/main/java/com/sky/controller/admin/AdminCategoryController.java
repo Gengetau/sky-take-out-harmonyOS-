976f73cd298a514b5a195e447f086ba4ff2e844b
gengetsu <x1156798139@gmail.com>
2025-12-11 10:17:05 +0800

    feat: 新增分类信息修改功能
    - 新增 PUT /admin/category 接口，用于根据ID修改分类信息。
    
    - 在 CategoryService 及 CategoryServiceImpl 中增加 updateCategory 方法，并添加事务支持。
    
    - Service层逻辑中，如果根据id查询不到分类，则会抛出 CategoryNotFoundException 自定义异常。
    
    - Category 实体类中的公共字段（如创建时间、更新用户等）已添加自动填充注解。

--- backend/sky-server/src/main/java/com/sky/controller/admin/AdminCategoryController.java
+++ backend/sky-server/src/main/java/com/sky/controller/admin/AdminCategoryController.java
@@ -1,14 +1,13 @@
 package com.sky.controller.admin;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.result.Result;
 import com.sky.service.CategoryService;
 import com.sky.vo.CategoryVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Gengetsu
@@ -32,5 +31,10 @@ public class AdminCategoryController {
 	@GetMapping("/page")
 	public Result<Page<CategoryVO>> getCategoryByPage(CategoryPageQueryDTO pageDTO) {
 		return categoryService.getCategoryByPage(pageDTO);
+	}
+	
+	@PutMapping
+	public Result<CategoryVO> updateCategory(@RequestBody CategoryDTO categoryDTO) {
+		return categoryService.updateCategory(categoryDTO);
 	}
 }
